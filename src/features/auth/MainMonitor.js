c076a6dbfcf927b6d121b1c4541cce3cc15f082e
Por123430 <74410037+Por123430@users.noreply.github.com>
2023-04-25 14:12:40 +0700

    Update MainMonitor.js

--- src/features/auth/MainMonitor.js
+++ src/features/auth/MainMonitor.js
@@ -3,16 +3,19 @@ import { db } from "../../firebase";
 import { ref, onValue } from "firebase/database";
 
 import "../../styles/monitor.css";
-const MainMonitor = () => {
 
-  const [sensor1Data, setSensor1Data] = useState([]);
+import temp from "../../img/celsius.png";
+import humi from "../../img/humidity2.png";
+import flame from "../../img/smoke-detector.png";
+const MainMonitor = () => {
+  const [todoData, setTodoData] = useState([]);
   const [sensor2Data, setSensor2Data] = useState([]);
 
   useEffect(() => {
     const starCountRef = ref(db, "ESP32/");
     onValue(starCountRef, (snapshot) => {
       const data = snapshot.val();
-      setSensor1Data(data);
+      setTodoData(data);
     });
   }, []);
 
@@ -23,8 +26,7 @@ const MainMonitor = () => {
       setSensor2Data(data);
     });
   }, []);
-
-      function flame(x){
+  function flame(x){
         if(x === 0){
           x = "undetect";
         } else {
@@ -32,39 +34,70 @@ const MainMonitor = () => {
         }
         return x;
       }
- 
+
   const content = (
     <div>
       <section className="LogMonitor">
         <div className="LogMonitor-display">
           <div className="LogMonitor-display__content">
             <div className="LogMonitor-display__item1">
-              <label>sensor 1 : Temperature</label>
-              {sensor1Data.temperature}
+              <div className="item-img">
+                <img src={temp} alt="imgtemp"></img>
+              </div>
+              <div className="item-content">
+                <label>sensor 1 : Temperature</label>
+                {todoData.temperature}
+              </div>
+
               {/* {imageData.photo} */}
             </div>
             <div className="LogMonitor-display__item1">
+            <div className="item-img">
+              <img src={humi} alt="imgtemp"></img>
+              </div>
+              <div className="item-content">
               <label>sensor 1 : Humidity</label>
-              {sensor1Data.humidity}
+              {todoData.humidity}</div>
             </div>
             <div className="LogMonitor-display__item1">
+            <div className="item-img">
+              <img src={flame} alt="imgtemp"></img>
+              </div>
+              <div className="item-content">
               <label>sensor 1 : flame</label>
               {flame(sensor1Data.flame)}
+              </div>
             </div>
           </div>
           <div className="LogMonitor-display__content">
             <div className="LogMonitor-display__item2">
+            <div className="item-img">
+              <img src={temp} alt="imgtemp"></img>
+              </div>
+              <div className="item-content">
               <label>sensor 2 : Temperature</label>
               {sensor2Data.temperature}
+              </div>
             </div>
             <div className="LogMonitor-display__item2">
+            <div className="item-img">
+              <img src={humi} alt="imgtemp"></img>
+              </div>
+              <div className="item-content">
               <label>sensor 2 : Humidity</label>
               {sensor2Data.humidity}
+              </div>
             </div>
             <div className="LogMonitor-display__item2">
+            <div className="item-img">
+              <img src={flame} alt="imgtemp"></img>
+              </div>
+              <div className="item-content">
               <label>sensor 2 : flame</label>
-              {flame(sensor2Data.flame)}
+             {flame(sensor2Data.flame)}
+              </div>
             </div>
+
           </div>
 
           <div className="LogMonitor-display__graph">
@@ -109,7 +142,7 @@ const MainMonitor = () => {
                 }}
                 width="410"
                 height="340"
-                src="https://charts.mongodb.com/charts-arduinoproject-dvaqg/embed/charts?id=643847dc-f155-4b1b-8b1f-cc909507c2c8&maxDataAge=300&theme=light&autoRefresh=true"
+                src="https://charts.mongodb.com/charts-arduinoproject-dvaqg/embed/charts?id=643847dc-f155-4b1b-8b1f-cc909507c2c8&maxDataAge=3600&theme=light&autoRefresh=true"
               ></iframe>
             </div>
           </div>
@@ -121,4 +154,3 @@ const MainMonitor = () => {
 };
 
 export default MainMonitor;
-
