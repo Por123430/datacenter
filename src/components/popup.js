1f05d463d48d99bfdec2392eccfcbcb793c6991c
Por123430 <74410037+Por123430@users.noreply.github.com>
2023-04-25 16:11:03 +0700

    Update popup.js

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import '../styles/popup.css'
+import React from "react";
+import "../styles/popup.css";
+import sound3 from "../sound/Radar_-_iPhone_Ringtone.mp4";
 
+function popup(props) {
+  function play3() {
+    new Audio(sound3).play();
+  }
+  return props.trigger ? (
+    <div className="popup" allow="autoplay">
+      <div className="popup-inner">
+        {play3()}
 
-function popup(props){
-  return (props.trigger) ? (
-    
-    <div className='popup'>
-        <div className='popup-inner'>
-        
-        
-            <div className='title'>
-
-            
-            { props.children }
-            </div>
-            <button className='close-btn' onClick={() => props.setTrigger(false)}
-            >close</button>
-            
-        </div>
+        <div className="title">{props.children}</div>
+        <button className="close-btn" onClick={() => props.setTrigger(false)}>
+          close
+        </button>
+      </div>
     </div>
-  ) : "";
+  ) : (
+    ""
+  );
 }
 
-export default popup
+export default popup;
