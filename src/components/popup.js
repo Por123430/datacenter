2f04a32ad9c3536e98030397355d3f4f7f08d0ad
Por123430 <74410037+Por123430@users.noreply.github.com>
2023-04-26 18:04:53 +0700

    Update popup.js

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import '../styles/popup.css'
 import alert from '../img/alert.png'
+import sound3 from "../sound/Radar_-_iPhone_Ringtone.mp4";
 
 function popup(props){
+  function play3() {
+    new Audio(sound3).play();
+  }
   return (props.trigger) ? (
     
     <div className='popup'>
         <div className='popup-inner'>
+    {play3()}
           <img src={alert} alt='alert'></img>
         
             <div className='title'>
