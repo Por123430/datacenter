730650688fe6310ec8d3f0788986a9aeaec3e802
Por123430 <74410037+Por123430@users.noreply.github.com>
2023-04-26 18:09:35 +0700

    Update Notihumi.js

--- src/features/notification/noti_humi/Notihumi.js
+++ src/features/notification/noti_humi/Notihumi.js
@@ -5,12 +5,15 @@ import "../../../styles/Table.css"
 const Notihumi = ({ notihumiId }) => {
     const notihumi = useSelector(state => selectNotiHumiditiesById(state, notihumiId));
     if (notihumi){
+        
+  const date = new Date(notihumi.createdAt);
+  const formattedDate = date.toLocaleString();
     const cellStatus = notihumi.active ? '' : 'table__cell--inactive'
 
     return (
       <tr className="table-allcell">
         <td className={`table-cell ${cellStatus}`}>{notihumi.humidity}</td>
-        <td className={`table-cell ${cellStatus}`}>{notihumi.savetime}</td>
+        <td className={`table-cell ${cellStatus}`}>{formattedDate}</td>
         
 
       </tr>
