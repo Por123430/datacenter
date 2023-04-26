3da1085db055adbfe1bafc1603dd89187125ef1d
Por123430 <74410037+Por123430@users.noreply.github.com>
2023-04-26 18:14:17 +0700

    Update Notitemp.js

--- src/features/notification/noti_temp/Notitemp.js
+++ src/features/notification/noti_temp/Notitemp.js
@@ -6,12 +6,14 @@ import { selectNotiTemperatureById } from './notitempApiSlice'
 const Notitemp = ({notitempId}) => {
   const notitemp = useSelector(state => selectNotiTemperatureById(state, notitempId));
   if (notitemp){
+     const date = new Date(notitemp.createdAt);
+  const formattedDate = date.toLocaleString();
     const cellStatus = notitemp.active ? '' : 'table__cell--inactive'
 
     return (
       <tr className="table-allcell">
         <td className={`table-cell ${cellStatus}`}>{notitemp.temperature}</td>
-        <td className={`table-cell ${cellStatus}`}>{notitemp.savetime}</td>
+        <td className={`table-cell ${cellStatus}`}>{formattedDate}</td>
         
 
       </tr>
